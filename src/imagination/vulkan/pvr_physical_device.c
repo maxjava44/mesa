85cc11f8ea156071a1d67d451b39aa6d2d4c1caa
maximumdeveloping <max.gilsoul@live.com>
2026-05-23 10:33:09 +0200

    Update pvr_physical_device.c

--- src/imagination/vulkan/pvr_physical_device.c
+++ src/imagination/vulkan/pvr_physical_device.c
@@ -768,9 +768,9 @@ static bool pvr_physical_device_get_properties(
       .driverInfo = "Mesa " PACKAGE_VERSION MESA_GIT_SHA1,
       .conformanceVersion = {
          .major = 1,
-         .minor = 4,
-         .subminor = 3,
-         .patch = 3,
+         .minor = 3,
+         .subminor = 8,
+         .patch = 4,
       },
 
       /* VK_EXT_extended_dynamic_state3 */
